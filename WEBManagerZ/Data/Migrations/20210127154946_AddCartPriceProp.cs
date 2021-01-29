f8626e809a7b9414649f26eb2d0549c89ce5682b
viktor111 <swifrorlilko@gmail.com>
2021-01-29 10:24:41 +0200

    ADD

---
+++ WEBManagerZ/Data/Migrations/20210127154946_AddCartPriceProp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEBManagerZ.Migrations
+{
+    public partial class AddCartPriceProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Carts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Carts");
+        }
+    }
+}
